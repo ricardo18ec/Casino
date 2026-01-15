664feed2f545b9e35863efe6efae24859913ce26
juancarlos200 <jk6610687@gmai.com>
2026-01-15 12:48:17 -0600

    metodos de dealer

--- Dealer.java
+++ Dealer.java
@@ -1,10 +1,26 @@
 public class Dealer {
     
+    //Artibutos
+
     public String Nombre;
     public int Edad;
     public Cartas Carta;
     private int Fichas;
-    
-    
 
+    //Metodos
+    public void mezclarCartas(){
+        // Metodo para mezclar las cartas
+    }
+    public void entregarCartas(){
+        // Metodo para entregar cartas a los jugadores
+    }
+    public void entregarFichas(){
+        // Metodo para entregar fichas al jugador
+    }
+    public void recibirApuestas(){
+        // Metodo para recibir apuestas de los jugadores
+    }
+    public void comenzarJuego(){
+        // Metodo para comenzar el juego
+    }
 }
