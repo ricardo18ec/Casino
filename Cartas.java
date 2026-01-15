8b77e57f340f612f618df7029bb14237d77e828c
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2026-01-15 12:30:53 -0600

    Clase Cartas.

--- Cartas.java
+++ Cartas.java
@@ -1,3 +1,17 @@
 public class Cartas {
-    
+    private int valor;
+    private String figura;
+
+    public Cartas(int valor, String figura) {
+        this.valor = valor;
+        this.figura = figura;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getFigura() {
+        return figura;
+    }
 }
