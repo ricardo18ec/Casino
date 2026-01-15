054077cdcb69df4a221382438e284b8502c4a589
juancarlos200 <jk6610687@gmai.com>
2026-01-15 12:31:55 -0600

    atributos interfaz

--- Juego.java
+++ Juego.java
@@ -1,5 +1,16 @@
 public class Juego {
     
-    public String Nombre;
+    // Este no contiene atributos ya que esto es solo una interfaz.
+
+
+    // Atributos
+    public repartirCartas(): 
+    public determinarGanador():Ganador():
+    public comenzarJuego():
+    public terminarJuego():
+    public pausarJuego():
+    public entregarPremio():
+    public realizarApuesta(double: monto)
+    public validarApuesta(double: monto)
 
 }
