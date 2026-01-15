abc7311d9df4e23f82173159f00b544a287c4f51
juancarlos200 <jk6610687@gmai.com>
2026-01-15 12:45:49 -0600

    metodos de poker

--- poker.java
+++ poker.java
@@ -1,11 +1,35 @@
 public class poker {
     
+    // atributos
+
     private String Nombre;
     private int numeroJugadores;
     private Dealer dealer;
     private Jugador[] jugadores;
     private Cartas[] cartas;
     private int cantidadMinimaApostar;
-    
 
+    //metodos
+    public void repartirCartas(){
+        // Metodo para repartir cartas a los jugadores
+    }
+    public String determinarGanador(){
+        // Metodo para determinar el ganador del juego
+        return "Ganador";
+    }
+    public void comenzarJuego(){
+        // Metodo para comenzar el juego
+    }
+    public void terminarJuego(){
+        // Metodo para terminar el juego
+    }
+    public void pausarJuego(){
+        // Metodo para pausar el juego
+    }
+    public void entregarPremio(){
+        // Metodo para entregar premios al ganador
+    }
+    public void pedirApuestas(){
+        // Metodo para pedir apuestas a los jugadores
+    }
 }
