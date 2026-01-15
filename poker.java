3e02d969e4a5c66f697e3cf007aab3517731cd6e
juancarlos200 <jk6610687@gmai.com>
2026-01-15 12:29:49 -0600

    atributos poker

--- poker.java
+++ poker.java
@@ -1,3 +1,11 @@
 public class poker {
     
+    private String Nombre;
+    private int numeroJugadores;
+    private Dealer dealer;
+    private Jugador[] jugadores;
+    private Cartas[] cartas;
+    private int cantidadMinimaApostar;
+    
+
 }
