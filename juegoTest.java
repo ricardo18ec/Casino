39b8bbec7218564c9dc72eede16ae0c43dc323c9
juancarlos200 <jk6610687@gmai.com>
2026-01-22 10:58:24 -0600

    test

--- juegoTest.java
+++ juegoTest.java
@@ -1,4 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class juegoTest {
-    //test para validar que la cantidad de la apuesta sea igual o mayor
-    //a la cantidad minima que se acepta para empezar a apostar 
+
+
+public class JuegoTest {
+
+    @Test
+    void apostar_debeFallar_siEsMenorAlMinimo() {
+        Juego juego = new Juego();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            juego.apostar(30.0);
+        });
+    }
+
+    @Test
+    void apostar_debePermitir_siEsIgualAlMinimo() {
+        Juego juego = new Juego();
+
+        assertDoesNotThrow(() -> {
+            juego.apostar(50.0);
+        });
+    }
+
+    @Test
+    void apostar_debePermitir_siEsMayorAlMinimo() {
+        Juego juego = new Juego();
+
+        assertDoesNotThrow(() -> {
+            juego.apostar(100.0);
+        });
+    }
+}
+
 }
